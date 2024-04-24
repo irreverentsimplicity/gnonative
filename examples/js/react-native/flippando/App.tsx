63ff13fe52fd05d38757122338c720e81e21d14d
irreverentsimplicity <irreverentsimplicity@proton.me>
2024-04-24 07:18:22 -0400

    refactoring, wip, not working

--- examples/js/react-native/flippando/App.tsx
+++ examples/js/react-native/flippando/App.tsx
@@ -24,7 +24,7 @@ export default function App() {
         console.warn("res ", JSON.stringify(res))
       })
       .catch((err) => setBoard("error " + JSON.stringify(err)));
-  }, []);
+  }, []);r
 
 
   return (
